a808af85225e875f60339f6302405a48f31f85b1
cengizozel <cozel@u.rochester.edu>
2023-05-27 05:47:37 -0400

    removed dist and build

--- build/lib/logidrivepy/__init__.py
+++
@@ -1,4 +0,0 @@
-from .constants import LogitechControllerConstants
-from .structs import LogitechControllerStructs
-from .functions import LogitechControllerFunctions
-from .controller import LogitechController
